892a1474c9390f1d6feb0a6a48631e339c34eec4
romainducrocq <romain.g.ducrocq@gmail.com>
2026-07-06 14:45:23 +0300

    fix dirty build

--- make.sh
+++ make.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 cd wheelcc/
-find . -name "*.o" -type f -delete
-if [ ${?} -ne 0 ]; then exit 1; fi
-rm -r src/ include/ lib/ build/build_cache/ build/build.sh bin/pkgname.conf
-if [ ${?} -ne 0 ]; then exit 1; fi
+find . -name "*.o" -type f -delete; if [ ${?} -ne 0 ]; then exit 1; fi
+if [ -d "src/" ]; then rm -r src/; fi; if [ ${?} -ne 0 ]; then exit 1; fi
+if [ -d "include/" ]; then rm -r include/; fi; if [ ${?} -ne 0 ]; then exit 1; fi
+if [ -d "lib/" ]; then rm -r lib/; fi; if [ ${?} -ne 0 ]; then exit 1; fi
+if [ -d "build/build.sh/" ]; then rm -r build/build.sh/; fi; if [ ${?} -ne 0 ]; then exit 1; fi
+if [ -f "build/build.sh" ]; then rm build/build.sh; fi; if [ ${?} -ne 0 ]; then exit 1; fi
+if [ -f "bin/pkgname.cfg" ]; then rm bin/pkgname.cfg; fi; if [ ${?} -ne 0 ]; then exit 1; fi
 
 cp -r ../src ../include ../lib .
 if [ ${?} -ne 0 ]; then exit 1; fi
 cp ../build.sh build/
 if [ ${?} -ne 0 ]; then exit 1; fi
-cp ../pkgname.conf bin/
+cp ../pkgname.cfg bin/
 if [ ${?} -ne 0 ]; then exit 1; fi
 
 cd bin/

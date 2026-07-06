53611230f1697e4d1b21f2524ab31a1ea421684b
romainducrocq <romain.g.ducrocq@gmail.com>
2026-07-06 13:04:55 +0300

    sds ported to wacc

---
+++ lib/sds/sds.c
@@ -0,0 +1,536 @@
+/* SDSLib 2.0 -- A C dynamic strings library
+ *
+ * Copyright (c) 2006-2015, Salvatore Sanfilippo <antirez at gmail dot com>
+ * Copyright (c) 2015, Oran Agra
+ * Copyright (c) 2015, Redis Labs, Inc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Redis nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "sds.h"
+
+// TODO
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wbuiltin-declaration-mismatch"
+extern void* malloc(unsigned long size);
+extern void* realloc(void* ptr, unsigned long size);
+extern void free(void* ptr);
+extern void* memset(void* s, int c, unsigned long n);
+extern void* memcpy(void* s1, void* s2, unsigned long n);
+extern void* memmove(void* s1, void* s2, unsigned long n);
+extern unsigned long strlen(char* s);
+#pragma GCC diagnostic pop
+//
+
+/* Note: sdshdr5 is never used, we just access the flags byte directly.
+ * However is here to document the layout of type 5 SDS strings. */
+struct sdshdr5 {
+    unsigned char flags; /* 3 lsb of type, and 5 msb of string length */
+    // char buf[];
+};
+struct sdshdr8 {
+    unsigned char len;   /* used */
+    unsigned char alloc; /* excluding the header and null terminator */
+    unsigned char flags; /* 3 lsb of type, 5 unused bits */
+    // char buf[];
+};
+struct sdshdr32 {
+    unsigned int len;    /* used */
+    unsigned int alloc;  /* excluding the header and null terminator */
+    unsigned char flags; /* 3 lsb of type, 5 unused bits */
+    // char buf[];
+};
+struct sdshdr64 {
+    unsigned long len;   /* used */
+    unsigned long alloc; /* excluding the header and null terminator */
+    unsigned char flags; /* 3 lsb of type, 5 unused bits */
+    // char buf[];
+};
+
+#define SDS_TYPE_5 0
+#define SDS_TYPE_8 1
+#define SDS_TYPE_32 3
+#define SDS_TYPE_64 4
+#define SDS_TYPE_MASK 7
+#define SDS_TYPE_BITS 3
+#define SDS_HDR_VAR(T, s) struct sdshdr##T* sh = (void*)((s) - (sizeof(struct sdshdr##T)));
+#define SDS_HDR(T, s) ((struct sdshdr##T*)((s) - (sizeof(struct sdshdr##T))))
+#define SDS_TYPE_5_LEN(f) ((f) >> SDS_TYPE_BITS)
+
+char* SDS_NOINIT = "SDS_NOINIT";
+
+unsigned long sdslen(sds s) {
+    unsigned char flags = s[-1];
+    switch (flags & SDS_TYPE_MASK) {
+        case SDS_TYPE_5:
+            return SDS_TYPE_5_LEN(flags);
+        case SDS_TYPE_8:
+            return SDS_HDR(8, s)->len;
+        case SDS_TYPE_32:
+            return SDS_HDR(32, s)->len;
+        case SDS_TYPE_64:
+            return SDS_HDR(64, s)->len;
+    }
+    return 0;
+}
+
+static unsigned long sdsavail(sds s) {
+    unsigned char flags = s[-1];
+    switch (flags & SDS_TYPE_MASK) {
+        case SDS_TYPE_5: {
+            return 0;
+        }
+        case SDS_TYPE_8: {
+            SDS_HDR_VAR(8, s);
+            return sh->alloc - sh->len;
+        }
+        case SDS_TYPE_32: {
+            SDS_HDR_VAR(32, s);
+            return sh->alloc - sh->len;
+        }
+        case SDS_TYPE_64: {
+            SDS_HDR_VAR(64, s);
+            return sh->alloc - sh->len;
+        }
+    }
+    return 0;
+}
+
+static void sdssetlen(sds s, unsigned long newlen) {
+    unsigned char flags = s[-1];
+    switch (flags & SDS_TYPE_MASK) {
+        case SDS_TYPE_5: {
+            unsigned char* fp = ((unsigned char*)s) - 1;
+            *fp = SDS_TYPE_5 | (newlen << SDS_TYPE_BITS);
+        } break;
+        case SDS_TYPE_8:
+            SDS_HDR(8, s)->len = newlen;
+            break;
+        case SDS_TYPE_32:
+            SDS_HDR(32, s)->len = newlen;
+            break;
+        case SDS_TYPE_64:
+            SDS_HDR(64, s)->len = newlen;
+            break;
+    }
+}
+
+static void sdssetalloc(sds s, unsigned long newlen) {
+    unsigned char flags = s[-1];
+    switch (flags & SDS_TYPE_MASK) {
+        case SDS_TYPE_5:
+            /* Nothing to do, this type has no total allocation info. */
+            break;
+        case SDS_TYPE_8:
+            SDS_HDR(8, s)->alloc = newlen;
+            break;
+        case SDS_TYPE_32:
+            SDS_HDR(32, s)->alloc = newlen;
+            break;
+        case SDS_TYPE_64:
+            SDS_HDR(64, s)->alloc = newlen;
+            break;
+    }
+}
+
+static int sdsHdrSize(char type) {
+    switch (type & SDS_TYPE_MASK) {
+        case SDS_TYPE_5:
+            return sizeof(struct sdshdr5);
+        case SDS_TYPE_8:
+            return sizeof(struct sdshdr8);
+        case SDS_TYPE_32:
+            return sizeof(struct sdshdr32);
+        case SDS_TYPE_64:
+            return sizeof(struct sdshdr64);
+    }
+    return 0;
+}
+
+static char sdsReqType(unsigned long string_size) {
+    if (string_size < 1 << 5)
+        return SDS_TYPE_5;
+    if (string_size < 1 << 8)
+        return SDS_TYPE_8;
+    if (string_size < 1l << 32)
+        return SDS_TYPE_32;
+    return SDS_TYPE_64;
+}
+
+/* Create a new sds string with the content specified by the 'init' pointer
+ * and 'initlen'.
+ * If 0 is used for 'init' the string is initialized with zero bytes.
+ * If SDS_NOINIT is used, the buffer is left uninitialized;
+ *
+ * The string is always null-terminated (all the sds strings are, always) so
+ * even if you create an sds string with:
+ *
+ * mystring = sdsnewlen("abc",3);
+ *
+ * You can print the string with printf() as there is an implicit \0 at the
+ * end of the string. However the string is binary safe and can contain
+ * \0 characters in the middle, as the length is stored in the sds header. */
+static sds sdsnewlen(void* init, unsigned long initlen) {
+    void* sh;
+    sds s;
+    char type = sdsReqType(initlen);
+    /* Empty strings are usually created in order to append. Use type 8
+     * since type 5 is not good at this. */
+    if (type == SDS_TYPE_5 && initlen == 0)
+        type = SDS_TYPE_8;
+    int hdrlen = sdsHdrSize(type);
+    unsigned char* fp; /* flags pointer. */
+
+    sh = malloc(hdrlen + initlen + 1);
+    if (sh == 0)
+        return 0;
+    if (init == SDS_NOINIT)
+        init = 0;
+    else if (!init)
+        memset(sh, 0, hdrlen + initlen + 1);
+    s = (char*)sh + hdrlen;
+    fp = ((unsigned char*)s) - 1;
+    switch (type) {
+        case SDS_TYPE_5: {
+            *fp = type | (initlen << SDS_TYPE_BITS);
+            break;
+        }
+        case SDS_TYPE_8: {
+            SDS_HDR_VAR(8, s);
+            sh->len = initlen;
+            sh->alloc = initlen;
+            *fp = type;
+            break;
+        }
+        case SDS_TYPE_32: {
+            SDS_HDR_VAR(32, s);
+            sh->len = initlen;
+            sh->alloc = initlen;
+            *fp = type;
+            break;
+        }
+        case SDS_TYPE_64: {
+            SDS_HDR_VAR(64, s);
+            sh->len = initlen;
+            sh->alloc = initlen;
+            *fp = type;
+            break;
+        }
+    }
+    if (initlen && init)
+        memcpy(s, init, initlen);
+    s[initlen] = 0;
+    return s;
+}
+
+/* Create a new sds string starting from a null terminated C string. */
+sds sdsnew(char* init) {
+    unsigned long initlen = (init == 0) ? 0 : strlen(init);
+    return sdsnewlen(init, initlen);
+}
+
+/* Duplicate an sds string. */
+sds sdsdup(sds s) { return sdsnewlen(s, sdslen(s)); }
+
+/* Free an sds string. No operation is performed if 's' is 0. */
+void sdsfree(sds s) {
+    if (s == 0)
+        return;
+    free((char*)s - sdsHdrSize(s[-1]));
+}
+
+/* Modify an sds string in-place to make it empty (zero length).
+ * However all the existing buffer is not discarded but set as free space
+ * so that next append operations will not require allocations up to the
+ * number of bytes previously available. */
+void sdsclear(sds s) {
+    sdssetlen(s, 0);
+    s[0] = 0;
+}
+
+/* Enlarge the free space at the end of the sds string so that the caller
+ * is sure that after calling this function can overwrite up to addlen
+ * bytes after the end of the string, plus one more byte for nul term.
+ *
+ * Note: this does not change the *length* of the sds string as returned
+ * by sdslen(), but only the free buffer space we have. */
+#define SDS_MAX_PREALLOC (1024 * 1024)
+sds sdsMakeRoomFor(sds s, unsigned long addlen) {
+    void* sh;
+    void* newsh;
+    unsigned long avail = sdsavail(s);
+    unsigned long len;
+    unsigned long newlen;
+    char type;
+    char oldtype = s[-1] & SDS_TYPE_MASK;
+    int hdrlen;
+
+    /* Return ASAP if there is enough space left. */
+    if (avail >= addlen)
+        return s;
+
+    len = sdslen(s);
+    sh = (char*)s - sdsHdrSize(oldtype);
+    newlen = (len + addlen);
+    if (newlen < SDS_MAX_PREALLOC)
+        newlen *= 2;
+    else
+        newlen += SDS_MAX_PREALLOC;
+
+    type = sdsReqType(newlen);
+
+    /* Don't use type 5: the user is appending to the string and type 5 is
+     * not able to remember empty space, so sdsMakeRoomFor() must be called
+     * at every appending operation. */
+    if (type == SDS_TYPE_5)
+        type = SDS_TYPE_8;
+
+    hdrlen = sdsHdrSize(type);
+    // assert(hdrlen + newlen + 1 > (len + addlen)); /* Catch unsigned long overflow */
+    if (oldtype == type) {
+        newsh = realloc(sh, hdrlen + newlen + 1);
+        if (newsh == 0)
+            return 0;
+        s = (char*)newsh + hdrlen;
+    }
+    else {
+        /* Since the header size changes, need to move the string forward,
+         * and can't use realloc */
+        newsh = malloc(hdrlen + newlen + 1);
+        if (newsh == 0)
+            return 0;
+        memcpy((char*)newsh + hdrlen, s, len + 1);
+        free(sh);
+        s = (char*)newsh + hdrlen;
+        s[-1] = type;
+        sdssetlen(s, len);
+    }
+    sdssetalloc(s, newlen);
+    return s;
+}
+
+/* Grow the sds to have the specified length. Bytes that were not part of
+ * the original length of the sds will be set to zero.
+ *
+ * if the specified length is smaller than the current length, no operation
+ * is performed. */
+sds sdsgrowzero(sds s, unsigned long len) {
+    unsigned long curlen = sdslen(s);
+
+    if (len <= curlen)
+        return s;
+    s = sdsMakeRoomFor(s, len - curlen);
+    if (s == 0)
+        return 0;
+
+    /* Make sure added region doesn't contain garbage */
+    memset(s + curlen, 0, (len - curlen + 1)); /* also set trailing \0 byte */
+    sdssetlen(s, len);
+    return s;
+}
+
+/* Append the specified binary-safe string pointed by 't' of 'len' bytes to the
+ * end of the specified sds string 's'.
+ *
+ * After the call, the passed sds string is no longer valid and all the
+ * references must be substituted with the new pointer returned by the call. */
+static sds sdscatlen(sds s, void* t, unsigned long len) {
+    unsigned long curlen = sdslen(s);
+
+    s = sdsMakeRoomFor(s, len);
+    if (s == 0)
+        return 0;
+    memcpy(s + curlen, t, len);
+    sdssetlen(s, curlen + len);
+    s[curlen + len] = 0;
+    return s;
+}
+
+/* Append the specified null termianted C string to the sds string 's'.
+ *
+ * After the call, the passed sds string is no longer valid and all the
+ * references must be substituted with the new pointer returned by the call. */
+sds sdscat(sds s, char* t) { return sdscatlen(s, t, strlen(t)); }
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstringop-overflow"
+#endif
+
+/* Helper for sdscatlonglong() doing the actual number -> string
+ * conversion. 's' must point to a string with room for at least
+ * SDS_LLSTR_SIZE bytes.
+ *
+ * The function returns the length of the null-terminated string
+ * representation stored at 's'. */
+#define SDS_LLSTR_SIZE 21
+static int sdsll2str(char* s, long value) {
+    char* p;
+    char aux;
+    unsigned long v;
+    unsigned long l;
+
+    /* Generate the string representation, this method produces
+     * an reversed string. */
+    if (value < 0) {
+        /* Since v is unsigned, if value==LLONG_MIN then
+         * -LLONG_MIN will overflow. */
+        if (value != -9223372036854775807l - 1) {
+            v = -value;
+        }
+        else {
+            v = ((unsigned long)9223372036854775807l) + 1;
+        }
+    }
+    else {
+        v = value;
+    }
+
+    p = s;
+    do {
+        *p++ = '0' + (v % 10);
+        v /= 10;
+    }
+    while (v);
+    if (value < 0)
+        *p++ = '-';
+
+    /* Compute length and add null term. */
+    l = p - s;
+    *p = 0;
+
+    /* Reverse the string. */
+    p--;
+    while (s < p) {
+        aux = *s;
+        *s = *p;
+        *p = aux;
+        s++;
+        p--;
+    }
+    return l;
+}
+
+/* Identical sdsll2str(), but for unsigned long type. */
+static int sdsull2str(char* s, unsigned long v) {
+    char* p;
+    char aux;
+    unsigned long l;
+
+    /* Generate the string representation, this method produces
+     * an reversed string. */
+    p = s;
+    do {
+        *p++ = '0' + (v % 10);
+        v /= 10;
+    }
+    while (v);
+
+    /* Compute length and add null term. */
+    l = p - s;
+    *p = 0;
+
+    /* Reverse the string. */
+    p--;
+    while (s < p) {
+        aux = *s;
+        *s = *p;
+        *p = aux;
+        s++;
+        p--;
+    }
+    return l;
+}
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic pop
+#endif
+
+/* Create an sds string from a long value. It is much faster than:
+ *
+ * sdscatprintf(sdsempty(),"%lld\n", value);
+ */
+sds sdsfromlonglong(long value) {
+    char buf[SDS_LLSTR_SIZE];
+    int len = sdsll2str(buf, value);
+
+    return sdsnewlen(buf, len);
+}
+
+sds sdsfromunsignedlonglong(unsigned long value) {
+    char buf[SDS_LLSTR_SIZE];
+    int len = sdsull2str(buf, value);
+
+    return sdsnewlen(buf, len);
+}
+
+/* Turn the string into a smaller (or equal) string containing only the
+ * substring specified by the 'start' and 'end' indexes.
+ *
+ * start and end can be negative, where -1 means the last character of the
+ * string, -2 the penultimate character, and so forth.
+ *
+ * The interval is inclusive, so the start and end characters will be part
+ * of the resulting string.
+ *
+ * The string is modified in-place.
+ *
+ * Example:
+ *
+ * s = sdsnew("Hello World");
+ * sdsrange(s,1,-1); => "ello World"
+ */
+void sdsrange(sds s, long start, long end) {
+    unsigned long newlen;
+    unsigned long len = sdslen(s);
+
+    if (len == 0)
+        return;
+    if (start < 0) {
+        start = len + start;
+        if (start < 0)
+            start = 0;
+    }
+    if (end < 0) {
+        end = len + end;
+        if (end < 0)
+            end = 0;
+    }
+    newlen = (start > end) ? 0 : (end - start) + 1;
+    if (newlen != 0) {
+        if (start >= (long)len) {
+            newlen = 0;
+        }
+        else if (end >= (long)len) {
+            end = len - 1;
+            newlen = (end - start) + 1;
+        }
+    }
+    if (start && newlen)
+        memmove(s, s + start, newlen);
+    s[newlen] = 0;
+    sdssetlen(s, newlen);
+}
---
+++ lib/sds/sds.h
@@ -0,0 +1,61 @@
+/* SDSLib 2.0 -- A C dynamic strings library
+ *
+ * Copyright (c) 2006-2015, Salvatore Sanfilippo <antirez at gmail dot com>
+ * Copyright (c) 2015, Oran Agra
+ * Copyright (c) 2015, Redis Labs, Inc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Redis nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __SDS_H
+#define __SDS_H
+
+// TODO
+#ifdef __clang__
+#pragma clang diagnostic ignored "-Wdiscarded-qualifiers"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
+#endif
+//
+
+#define sds char*
+
+unsigned long sdslen(sds s);
+sds sdsnew(char* init);
+sds sdsdup(sds s);
+void sdsfree(sds s);
+sds sdsgrowzero(sds s, unsigned long len);
+sds sdscat(sds s, char* t);
+
+void sdsrange(sds s, long start, long end);
+void sdsclear(sds s);
+sds sdsfromlonglong(long value);
+sds sdsfromunsignedlonglong(unsigned long value);
+
+/* Low level functions exposed to the user API */
+sds sdsMakeRoomFor(sds s, unsigned long addlen);
+
+#endif

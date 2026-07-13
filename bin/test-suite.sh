42a3144cb3408b8cb07389286eaff27a6f8440bf
romainducrocq <romain.g.ducrocq@gmail.com>
2026-07-13 20:04:38 +0300

    fail on unsupported FreeBSD

--- bin/test-suite.sh
+++ bin/test-suite.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+KERNEL_NAME="$(uname -s)"
 PROJECT_DIR="$(dirname $(readlink -f ${0}))"
 EXEC_NAME="$(cat ${PROJECT_DIR}/exec.name)"
 TEST_SUITE="${PROJECT_DIR}/../writing-a-c-compiler-tests"
+
+if [[ "${KERNEL_NAME}" == "FreeBSD"* ]]; then
+    echo -e "\033[0;31merror:\033[0m \033[1mthe test suite does not support FreeBSD" 1>&2
+    exit 1
+fi
 
 bash ${PROJECT_DIR}/set-exec.sh ${1}
 if [ ${?} -ne 0 ]; then exit 1; fi

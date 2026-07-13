33342e3cc2c47069ede9878121e41c049f0a5fcc
romainducrocq <romain.g.ducrocq@gmail.com>
2026-07-13 18:36:32 +0300

    reset exec in test suite

--- bin/test-suite.sh
+++ bin/test-suite.sh
@@ -1,27 +1,44 @@
 #!/usr/bin/env bash
 
 PROJECT_DIR="$(dirname $(readlink -f ${0}))"
+EXEC_NAME="$(cat ${PROJECT_DIR}/exec.name)"
 TEST_SUITE="${PROJECT_DIR}/../writing-a-c-compiler-tests"
 
-function test () {
+bash ${PROJECT_DIR}/set-exec.sh ${1}
+if [ ${?} -ne 0 ]; then exit 1; fi
+
+function reset_exec () {
+    cd ${PROJECT_DIR}
+    echo ""
+    bash ${PROJECT_DIR}/set-exec.sh ${EXEC_NAME}
+    return ${?}
+}
+
+function run_tests () {
     echo ""
     echo "----------------------------------------------------------------------"
-    echo "${@}"
+    echo "(${EXEC_NAME}) ${@}"
     ./test_compiler ${PROJECT_DIR}/driver.sh ${@}
-    if [ ${?} -ne 0 ]; then exit 1; fi
+    if [ ${?} -ne 0 ]; then
+        reset_exec
+        exit 1
+    fi
+    return 0
 }
 
 cd ${TEST_SUITE}
-
-if [ ${#} -ne 0 ]; then
-    test ${@}
+if [ ${#} -gt 1 ]; then
+    run_tests ${@:2}
 else
     for i in $(seq 1 18); do
-        test --chapter ${i} --latest-only --extra-credit
+        run_tests --chapter ${i} --latest-only --extra-credit
     done
     for i in $(seq 19 20); do
-        test --chapter ${i} --extra-credit
+        run_tests --chapter ${i} --extra-credit
     done
 fi
+
+reset_exec
+if [ ${?} -ne 0 ]; then exit 1; fi
 
 exit 0

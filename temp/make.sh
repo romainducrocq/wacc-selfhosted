781b9528edd9be8f1b9c15258fa040f91c083da6
romainducrocq <romain.g.ducrocq@gmail.com>
2026-07-13 09:07:18 +0300

    move build to temp

--- temp/make.sh
+++ temp/make.sh
@@ -2,7 +2,7 @@
 
 sudo -v
 
-cd wheelcc/
+cd ../wheelcc/
 find . -name "*.o" -type f -delete; if [ ${?} -ne 0 ]; then exit 1; fi
 if [ -d "src/" ]; then rm -r src/; fi; if [ ${?} -ne 0 ]; then exit 1; fi
 if [ -d "include/" ]; then rm -r include/; fi; if [ ${?} -ne 0 ]; then exit 1; fi
@@ -12,13 +12,13 @@ if [ -f "build/build.sh" ]; then rm build/build.sh; fi; if [ ${?} -ne 0 ]; then 
 if [ -f "bin/pkgname.cfg" ]; then rm bin/pkgname.cfg; fi; if [ ${?} -ne 0 ]; then exit 1; fi
 if [ -f "test/tools/print_errors.in" ]; then rm test/tools/print_errors.in; fi; if [ ${?} -ne 0 ]; then exit 1; fi
 
-cp -r ../src ../include ../lib .
+cp -r ../temp/src ../temp/include ../temp/lib .
 if [ ${?} -ne 0 ]; then exit 1; fi
-cp ../build.sh build/
+cp ../temp/build.sh build/
 if [ ${?} -ne 0 ]; then exit 1; fi
-cp ../pkgname.cfg bin/
+cp ../temp/pkgname.cfg bin/
 if [ ${?} -ne 0 ]; then exit 1; fi
-cp ../print_errors.in test/tools/
+cp ../temp/print_errors.in test/tools/
 if [ ${?} -ne 0 ]; then exit 1; fi
 
 # sed -i "s|IS_PREPROC=0|IS_PREPROC=1|g" bin/driver.sh

d02c88376288cced184dcd2439436382a4c3602f
romainducrocq <romain.g.ducrocq@gmail.com>
2026-07-14 15:29:31 +0300

    add MacOS Rosetta 2 warning

--- bin/test-suite.sh
+++ bin/test-suite.sh
@@ -5,7 +5,9 @@ PROJECT_DIR="$(dirname $(readlink -f ${0}))"
 EXEC_NAME="$(cat ${PROJECT_DIR}/exec.name)"
 TEST_SUITE="${PROJECT_DIR}/../writing-a-c-compiler-tests"
 
-if [[ "${KERNEL_NAME}" != "FreeBSD"* ]]; then
+if [[ "${KERNEL_NAME}" != "Darwin"* ]]; then
+    echo -e "\033[1;34mwarning:\033[0m run \033[1m‘arch -x86_64 zsh’\033[0m first to invoke this script under Rosetta 2 on aarch64 MacOS"
+elif [[ "${KERNEL_NAME}" == "FreeBSD"* ]]; then
     echo -e "test-suite: \033[0;31merror:\033[0m the test suite does not support FreeBSD, try to selfhost the compiler instead!" 1>&2
     exit 1
 fi
@@ -27,10 +29,7 @@ function run_tests () {
     echo "----------------------------------------------------------------------"
     echo "(${EXEC_NAME}) ${@}"
     ./test_compiler ${PROJECT_DIR}/driver.sh ${@}
-    if [ ${?} -ne 0 ]; then
-        reset_exec
-        exit 1
-    fi
+    if [ ${?} -ne 0 ]; then reset_exec; exit 1; fi
     return 0
 }
 
@@ -44,6 +43,9 @@ bash ${PROJECT_DIR}/set-exec.sh ${1}
 if [ ${?} -ne 0 ]; then exit 1; fi
 
 cd ${TEST_SUITE}
+./test_compiler --check-setup
+if [ ${?} -ne 0 ]; then reset_exec; exit 1; fi
+
 if [ ${#} -gt 1 ]; then
     run_tests ${@:2}
 else

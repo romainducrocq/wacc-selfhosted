8812095ce6ce0915f2c45b31b4ecdb5b06298b72
romainducrocq <romain.g.ducrocq@gmail.com>
2026-07-14 10:47:56 +0300

    build executable

--- bin/build.sh
+++ bin/build.sh
@@ -14,6 +14,35 @@ CC_FLAGS_RELEASE="-O3 -DNDEBUG -Werror -pedantic-errors -D__GCC_STDINT__"
 CC_FLAGS="${CC_FLAGS} ${CC_FLAGS_RELEASE}"
 LD="${CC}"
 
+BUILD_DIR="$(dirname ${PROJECT_DIR})/build"
+COMPILER_DIR="$(dirname ${PROJECT_DIR})/compiler"
+
+SOURCE_FILES="${COMPILER_DIR}/lib/main.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/3rdparty/sds.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/3rdparty/stb_ds.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/ast.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/back_ast.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/back_symt.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/front_ast.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/front_symt.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/interm_ast.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/backend/asm_gen.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/backend/gas_code.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/backend/registers.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/backend/stack_fix.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/backend/symt_cvt.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/errors.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/idents.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/lexer.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/parser.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/semantic.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/tac_repr.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/optimizer/optim_tac.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/optimizer/reg_alloc.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/util/fileio.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/util/str2t.c"
+SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/util/throw.c"
+
 function help () {
     # TODO
     exit 0
@@ -49,7 +78,7 @@ function build_preset () {
             fi
             ;;
         *)
-            return 1
+            raise_error "$(em "${LD}") is not a valid linker"
     esac
 
     as --help > /dev/null 2>&1
@@ -59,85 +88,66 @@ function build_preset () {
     return 0
 }
 
-build_preset
+function build_exec () {
+    if [ -d "${BUILD_DIR}/" ]; then
+        rm -r ${BUILD_DIR}/
+        if [ ${?} -ne 0 ]; then return 1; fi
+    fi
+    mkdir ${BUILD_DIR}/
+    if [ ${?} -ne 0 ]; then return 1; fi
 
+    OBJECT_FILES=""
+    echo "-- Build objects ..."
+    for FILE in ${SOURCE_FILES}; do
+        OBJECT="${BUILD_DIR}/$(basename ${FILE%.*}).o"
+        OBJECT_FILES="${OBJECT_FILES} ${OBJECT}"
+        echo "(${CC}) ${FILE} -> ${OBJECT}"
+
+        # wheelcc -E -c ${FILE}
+        # if [ ${?} -ne 0 ]; then return 1; fi
+        # mv -v ${FILE%.*}.o ${OBJECT}
+        # if [ ${?} -ne 0 ]; then return 1; fi
+
+        ${CC} -c ${FILE} ${CC_FLAGS} -o ${OBJECT}
+        if [ ${?} -ne 0 ]; then return 1; fi
+    done
+    echo "OK"
+
+    echo "-- Linking executable ..."
+    echo "${BUILD_DIR}/*.o -> ${PROJECT_NAME}"
+    ${LD} ${OBJECT_FILES} ${CC_FLAGS} -o ${PROJECT_NAME}
+    if [ ${?} -ne 0 ]; then return 1; fi
+    echo "OK"
+
+    EXEC_NAME="$(basename ${PROJECT_NAME})"
+    bash ${PROJECT_DIR}/set-exec.sh ${EXEC_NAME}
+    if [ ${?} -ne 0 ]; then exit 1; fi
+
+    echo "-- Created target ${PROJECT_NAME}"
+
+    find ${PROJECT_DIR}/ -maxdepth 1 -name wacc-selfhosted -type l -delete
+    if [ ${?} -ne 0 ]; then return 1; fi
+
+    ln -s ${PROJECT_DIR}/driver.sh ${PROJECT_DIR}/wacc-selfhosted
+    if [ ${?} -ne 0 ]; then return 1; fi
+
+    echo -e "-- Created symlink \033[1;36m${PROJECT_DIR}/wacc-selfhosted\033[0m -> \033[1;32m${PROJECT_DIR}/driver.sh\033[0m"
+    return 0
+}
+
+# TODO resolve project name
 PROJECT_NAME="${PROJECT_DIR}/wacc-selfhosted-1"
 if [ -f "${PROJECT_NAME}" ]; then
     rm ${PROJECT_NAME}
-    if [ ${?} -ne 0 ]; then exit 1; fi
+    if [ ${?} -ne 0 ]; then return 1; fi
 fi
 
-COMPILER_DIR="$(dirname ${PROJECT_DIR})/compiler"
+# TODO build wheelcc/nqcc2
 
-SOURCE_FILES="${COMPILER_DIR}/lib/main.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/3rdparty/sds.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/3rdparty/stb_ds.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/ast.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/back_ast.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/back_symt.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/front_ast.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/front_symt.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/ast/interm_ast.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/backend/asm_gen.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/backend/gas_code.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/backend/registers.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/backend/stack_fix.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/backend/symt_cvt.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/errors.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/idents.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/lexer.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/parser.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/semantic.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/frontend/tac_repr.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/optimizer/optim_tac.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/optimizer/reg_alloc.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/util/fileio.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/util/str2t.c"
-SOURCE_FILES="${SOURCE_FILES} ${COMPILER_DIR}/util/throw.c"
-
-BUILD_DIR="$(dirname ${PROJECT_DIR})/build"
-if [ -d "${BUILD_DIR}/" ]; then
-    rm -r ${BUILD_DIR}/
-    if [ ${?} -ne 0 ]; then exit 1; fi
-fi
-mkdir ${BUILD_DIR}/
+build_preset
 if [ ${?} -ne 0 ]; then exit 1; fi
+build_exec
+if [ ${?} -ne 0 ]; then raise_error "build failed"; fi
 
-OBJECT_FILES=""
-echo "-- Build objects ..."
-for FILE in ${SOURCE_FILES}; do
-    OBJECT="${BUILD_DIR}/$(basename ${FILE%.*}).o"
-    OBJECT_FILES="${OBJECT_FILES} ${OBJECT}"
-    echo "(${CC}) ${FILE} -> ${OBJECT}"
-
-    # wheelcc -v -E -c ${FILE}.c
-    # if [ ${?} -ne 0 ]; then exit 1; fi
-    # mv -v ${FILE}.o ${OBJECT}
-
-    ${CC} -c ${FILE} ${CC_FLAGS} -o ${OBJECT}
-    if [ ${?} -ne 0 ]; then exit 1; fi
-done
-echo "OK"
-
-echo "-- Linking executable ..."
-echo "${BUILD_DIR}/*.o -> ${PROJECT_NAME}"
-${LD} ${OBJECT_FILES} ${CC_FLAGS} -o ${PROJECT_NAME}
-if [ ${?} -ne 0 ]; then exit 1; fi
-echo "OK"
-
-EXEC_NAME="$(basename ${PROJECT_NAME})"
-bash ${PROJECT_DIR}/set-exec.sh ${EXEC_NAME}
-if [ ${?} -ne 0 ]; then exit 1; fi
-
-echo "-- Created target ${PROJECT_NAME}"
-
-find ${PROJECT_DIR}/ -maxdepth 1 -name wacc-selfhosted -type l -delete
-if [ ${?} -ne 0 ]; then exit 1; fi
-
-ln -s ${PROJECT_DIR}/driver.sh ${PROJECT_DIR}/wacc-selfhosted
-if [ ${?} -ne 0 ]; then exit 1; fi
-
-echo -e "-- Created symlink \033[1;36m${PROJECT_DIR}/wacc-selfhosted\033[0m -> \033[1;32m${PROJECT_DIR}/driver.sh\033[0m"
-echo -e "See usage with command \033[1m‘./wacc-selfhosted --help’\033[0m"
-
+echo -e "See usage with command $(em "./wacc-selfhosted --help")"
 exit 0

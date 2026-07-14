901814f7290d8f675f98b17f8b370a180ade21f5
romainducrocq <romain.g.ducrocq@gmail.com>
2026-07-14 10:02:43 +0300

    add error handling

--- bin/set-exec.sh
+++ bin/set-exec.sh
@@ -1,26 +1,38 @@
 #!/usr/bin/env bash
 
 PROJECT_DIR="$(dirname $(readlink -f ${0}))"
-
 EXEC_NAME="$(basename "${1}")"
+
+function help () {
+    # TODO
+    exit 0
+}
+
+function em() {
+    echo "\033[1m‘${1}’\033[0m"
+}
+
+function raise_error () {
+    ERROR_MESSAGE="${1}"
+    echo -e "set-exec: \033[0;31merror:\033[0m ${ERROR_MESSAGE}, see $(em "--help")" 1>&2
+    exit 1
+}
+
 echo -e "-- Set executable name ..."
 if [ ! -f "${PROJECT_DIR}/${EXEC_NAME}" ]; then
-    echo -e "\033[0;31merror:\033[0m \033[1m‘${EXEC_NAME}’\033[0m is not a valid executable name" 1>&2
-    exit 1
+    raise_error "$(em "${EXEC_NAME}") is not a valid executable name"
 fi
 
 for FILE in build.sh crt.s crt.o driver.sh exec.name set-exec.sh test-suite.sh wacc-selfhosted; do
     if [ "${EXEC_NAME}" = "${FILE}" ]; then
-        echo -e "\033[0;31merror:\033[0m \033[1m‘${EXEC_NAME}’\033[0m is not a valid executable name" 1>&2
-        exit 1
+        raise_error "$(em "${EXEC_NAME}") is not a valid executable name"
     fi
 done
 
 echo "${PROJECT_DIR}/exec.name = ${EXEC_NAME}"
 echo -n "${EXEC_NAME}" > ${PROJECT_DIR}/exec.name
 if [ ${?} -ne 0 ]; then
-    echo -e "\033[0;31merror:\033[0m set executable failed" 1>&2
-    exit 1
+    raise_error "set executable failed"
 fi
 
 echo "OK"
